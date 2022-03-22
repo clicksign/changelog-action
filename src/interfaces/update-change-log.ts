d418b50ad4f78713c48669b899f2c1947cf5665f
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2022-03-22 15:25:57 -0300

    devops: update change log

---
+++ src/interfaces/update-change-log.ts
@@ -0,0 +1,14 @@
+import {Context} from '@actions/github/lib/context'
+
+export default interface IUpdateChangelog {
+  toolkit: any
+  context: Context
+  changelogFileName: string
+  newLog: string
+  newComments?: string
+  logFind: string
+  commentFind: string
+  encoding: any
+  oldLogs: string
+  quantityLogs: number
+}
